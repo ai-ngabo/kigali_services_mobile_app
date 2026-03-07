981f0a8e47e12accd77ea4dec7d9611160b589c0
Alain Ishimwe NGABO <ngaboalan1@gmail.com>
2026-03-07 14:22:26 +0200

    provider to toggle to notification service instance

--- lib/providers/settings_provider.dart
+++ lib/providers/settings_provider.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/foundation.dart';
+import '../services/notification_service.dart';
 
 class SettingsProvider extends ChangeNotifier {
-  bool _notificationsEnabled = false;
+  bool _notificationsEnabled = true;
 
   bool get notificationsEnabled => _notificationsEnabled;
 
   void toggleNotifications() {
     _notificationsEnabled = !_notificationsEnabled;
+    NotificationService.instance.setEnabled(_notificationsEnabled);
     notifyListeners();
   }
 
   void setNotifications(bool value) {
     _notificationsEnabled = value;
+    NotificationService.instance.setEnabled(value);
     notifyListeners();
   }
 }
